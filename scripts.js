de9b336034e552d91657140de81f5cf9eccc18a1
trinityfurman <trinity.furman@gmail.com>
2021-08-27 11:26:41 -0700

    Create basic 16x16 grid

---
+++ scripts.js
@@ -0,0 +1,28 @@
+// Create variable for container
+const container = document.querySelector('#container');
+
+// Create 16 rows of 16 divs each
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+
+        const div = document.createElement('div');
+
+        // Calculate size based on number of columns and size of margin
+        let width = (960 - (2 * 16))/ 16;
+        
+        // For the first div of each row, clear to the next line
+        if (i > 0 && j == 0) {
+            div.classList.add('clear');
+            div.classList.add('float');
+        } else {
+            div.classList.add('float');
+        }
+        // Set divs to proper width and height with template literal
+        div.style.width = `${width}px`;
+        div.style.height = `${width}px`;
+        // Add new div to the container
+        container.appendChild(div);
+    }
+}
+
+
