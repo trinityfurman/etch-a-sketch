b6850c522cc2692b7b90af5cb64561705b7599e0
trinityfurman <trinity.furman@gmail.com>
2021-08-30 11:04:30 -0700

    Create button that allows user to create a new grid.

--- scripts.js
+++ scripts.js
@@ -1,14 +1,19 @@
 // Create variable for container
 const container = document.querySelector('#container');
 
-// Create 16 rows of 16 divs each
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
+// Initialize number of boxes to 16
+let boxes = 16;
+
+// Create grid of set size
+function createGrid(numberOfBoxes) {
+
+for (let i = 0; i < numberOfBoxes; i++) {
+    for (let j = 0; j < numberOfBoxes; j++) {
 
         const div = document.createElement('div');
 
         // Calculate size based on number of columns and size of margin
-        let width = (960 - (2 * 16))/ 16;
+        let width = (960 - (2 * numberOfBoxes))/ numberOfBoxes;
         
         // For the first div of each row, clear to the next line
         if (i > 0 && j == 0) {
@@ -27,8 +32,41 @@ for (let i = 0; i < 16; i++) {
         });
         // Add new div to the container
         container.appendChild(div);
-
     }
 }
+}
+
+// Create initial grid on page
+createGrid(boxes);
+
+// Create button constant
+const btn = document.querySelector('#btn');
+
+// Remove all grid divs when button is clicked
+btn.addEventListener('click', () => {
+    const divList = document.querySelectorAll('.float');
+    const length = divList.length;
+
+    // Loop through each div and remove it 
+    for (i = 0; i < length; i++){
+        divList[i].remove();
+    }
+
+    // Ask user for size
+    let userSize = parseInt(prompt("How many boxes wide?"));
+
+    // Ensure user size is an integer between 1 and 100
+    if (userSize < 1) {
+        userSize = parseInt(prompt("Please enter a number greater than 0! How many boxes wide?"));
+    } else if (userSize > 100) {
+        userSize = parseInt(prompt("Please enter a number under 100. How many boxes wide?"));
+    } else if (isNaN(userSize) == true) {
+        userSize = parseInt(prompt("Please enter an integer. How many boxes wide?"));
+    } else if (userSize != null) {
+        createGrid(userSize);
+    }
+});
+
+
 
 
