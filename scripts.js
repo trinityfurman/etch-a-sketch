7aeab01ba6d2885d234fca2378912d77e88d3d03
trinityfurman <trinity.furman@gmail.com>
2021-08-31 09:54:40 -0700

    Add rainbow background feature.

--- scripts.js
+++ scripts.js
@@ -1,16 +1,30 @@
-// Create variable for container
-const container = document.querySelector('#container');
 
-// Initialize number of boxes to 16
-let boxes = 16;
+// Generate random integer 
+function getRandomInteger(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); 
+  }
+
+// Generate random color 
+function getRandomColor() {
+    // Generate random integers for r, g, & b values
+    let r = getRandomInteger(0, 255);
+    let g = getRandomInteger(0, 255);
+    let b = getRandomInteger(0, 255);
+
+    let newColor = `rgb(${r}, ${g}, ${b})`;
+    return newColor;
+ }
+
 
 // Create grid of set size
 function createGrid(numberOfBoxes) {
-
-for (let i = 0; i < numberOfBoxes; i++) {
+ for (let i = 0; i < numberOfBoxes; i++) {
     for (let j = 0; j < numberOfBoxes; j++) {
 
         const div = document.createElement('div');
+        div.classList.add('grid');
 
         // Calculate size based on number of columns and size of margin
         let width = (960 - (2 * numberOfBoxes))/ numberOfBoxes;
@@ -33,8 +47,14 @@ for (let i = 0; i < numberOfBoxes; i++) {
         // Add new div to the container
         container.appendChild(div);
     }
+ }
 }
-}
+
+// Create variable for container
+const container = document.querySelector('#container');
+
+// Initialize number of boxes to 16
+let boxes = 16;
 
 // Create initial grid on page
 createGrid(boxes);
@@ -66,6 +86,24 @@ btn.addEventListener('click', () => {
         createGrid(userSize);
     }
 });
+
+// Create constant for rainbow button
+const rainbow = document.querySelector("#rainbow");
+// Add event listener to rainbow button
+rainbow.addEventListener('click', () => {
+    // Create nodelist of every div in grid 
+    const divList = document.querySelectorAll('.grid');
+    // Loop through each div and add random color background
+    divList.forEach((div) => {
+        div.addEventListener("mouseenter", () => {
+            div.style.backgroundColor = getRandomColor();
+        });
+     });
+ });
+
+
+
+ 
 
 
 
